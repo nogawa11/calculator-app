8b337b13336568c70792fa1f0fe2570bd98321b0
Nicole Ogawa <nogawa11@gmail.com>
2022-04-06 20:38:15 +0900

    fixed multiple operators error

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ const times = document.querySelector(".times");
 const divide = document.querySelector(".divide");
 const decimal = document.querySelector(".decimal");
 const equal = document.querySelector(".equal");
-let total = '';
-let firstNum = '';
-let secondNum = '';
-let totalAmount = '';
-let formattedAmount = '';
-let sign = '';
-let signTwo = '';
+let total = "";
+let firstNum = "";
+let secondNum = "";
+let formattedAmount = "";
+let sign = "";
+let signTwo = "";
+let check = [];
 const numbers = [one, two, three, four, five, six, seven, eight, nine, zero]
 const operators = [plus, minus, times, divide]
 
@@ -39,83 +39,88 @@ btns.forEach((btn) => {
 })
 
 const addComma = (num) => {
-  withoutComma = num.replace(/,/g, '');
-  number = Number(withoutComma);
-  return number.toLocaleString('en-US', { maximumFractionDigits: 15 });
+  withoutComma = num.replace(/,/g, "");
+  const number = Number(withoutComma);
+  return number.toLocaleString("en-US", { maximumFractionDigits: 15 });
 }
 
 const checkComma = (string) => {
-  let testString = string
+  let testString = string;
   if (/[,]/.test(string)) {
-    testString = string.replace(/,/g, '');
+    testString = string.replace(/,/g, "");
   }
   return testString;
 }
 
 const formula = (num1, num2, op) => {
-  let number = '';
-  if (op === '+') {
+  let number = "";
+  if (op === "+") {
     number = num1 + num2;
-  } else if (op === '-') {
+  } else if (op === "-") {
     number = num1 - num2;
-  } else if (op === 'x') {
+  } else if (op === "x") {
     number = num1 * num2;
-  } else if (op === '/') {
+  } else if (op === "/") {
     number = num1 / num2;
   }
-  return number;
+  return number.toLocaleString("en-US", { maximumFractionDigits: 15 });
 }
 
 const calculate = (op) => {
-  if (firstNum === '') {
-    firstNum = total
-    totalNum.innerText = 0;
-    total = '0'
-  } else if (firstNum !== '' && secondNum === '') {
-    secondNum = total
+  if (firstNum === "") {
+    firstNum = total;
+    totalNum.innerText = total;
+    total = "0";
+  } else if (firstNum !== "" && secondNum === "") {
+    secondNum = total;
     firstNum = checkComma(firstNum);
     secondNum = checkComma(secondNum);
     const first = Number(firstNum);
     const second = Number(secondNum);
-    totalAmount = formula(first, second, op);
-    formattedAmount = totalAmount.toLocaleString('en-US', { maximumFractionDigits: 15 });
+    formattedAmount = formula(first, second, op);
     if (formattedAmount.length > 15) {
       if (/[,]/.test(formattedAmount)) {
-        formattedAmount = formattedAmount.replace(/,/g, '');
+        formattedAmount = formattedAmount.replace(/,/g, "");
       }
       formattedAmount = formattedAmount.slice(0, 16);
       formattedAmount = addComma(formattedAmount);
-      if (formattedAmount.slice(-1) === '.') {
+      if (formattedAmount.slice(-1) === ".") {
         formattedAmount = formattedAmount.slice(0, -1);
       };
     }
     totalNum.innerText = formattedAmount;
-    firstNum = formattedAmount
-    secondNum = '';
-    total='';
+    firstNum = formattedAmount;
+    secondNum = "";
+    total="";
   }
 }
 
 operators.forEach((operator) => {
   operator.addEventListener("click", (event) => {
     event.preventDefault();
-    if (sign === '') {
-      sign = event.currentTarget.innerText;
-      calculate(sign);
-    } else if (sign !== '' && signTwo === '') {
+    check.push("sign");
+    if (check.at(-1) === "sign" && check.at(-2) === "sign" && check.includes("num")) {
       signTwo = event.currentTarget.innerText;
-      calculate(sign);
-    } else if (sign !== '' && signTwo !== '') {
-      sign = signTwo;
-      signTwo = event.currentTarget.innerText;
-      calculate(sign);
+      calculate(signTwo);
+    } else if (check.includes("num")) {
+      if (sign === "") {
+        sign = event.currentTarget.innerText;
+        calculate(sign);
+      } else if (sign !== "" && signTwo === "") {
+        signTwo = event.currentTarget.innerText;
+        calculate(sign);
+      } else if (sign !== "" && signTwo !== "") {
+        sign = signTwo;
+        signTwo = event.currentTarget.innerText;
+        calculate(sign);
+      }
     }
   })
 })
 
 equal.addEventListener("click", (event) => {
   event.preventDefault();
-  if (signTwo === '') {
+  if (signTwo === "") {
     calculate(sign);
   } else {
     calculate(signTwo);
@@ -125,7 +130,7 @@ equal.addEventListener("click", (event) => {
 numbers.forEach((number) => {
   number.addEventListener("click", (event) => {
     event.preventDefault();
-    if (totalNum.innerText === '0') {
+    if (totalNum.innerText === "0") {
       totalNum.innerText = event.currentTarget.innerText;
       total = event.currentTarget.innerText;
     } else if (total.length <= 15 || total.length === undefined) {
@@ -135,6 +140,7 @@ numbers.forEach((number) => {
       }
       totalNum.innerText = total;
     }
+    check.push("num");
   })
 })
 
@@ -143,29 +149,29 @@ decimal.addEventListener("click", (event) => {
   if ((/[\.]/.test(total))) {
     total = total;
     totalNum.innerText = total;
-  } else if (totalNum.innerText === '0' || total === '') {
-    totalNum.innerText = '0.';
-    total = '0.';
+  } else if (totalNum.innerText === "0" || total === "") {
+    totalNum.innerText = "0.";
+    total = "0.";
   } else if (total.length <= 15 || total.length === undefined) {
-    total = total + '.';
+    total = total + ".";
     totalNum.innerText = total;
   }
 })
 
 del.addEventListener("click", (event) => {
   event.preventDefault();
-  if (totalNum.innerText === '0') {
-    totalNum.innerText = '0';
-    total = '0';
+  if (totalNum.innerText === "0") {
+    totalNum.innerText = "0";
+    total = "0";
   } else if (total.length === 1 || total.length === undefined) {
-    totalNum.innerText = '0';
-    total = '0';
+    totalNum.innerText = "0";
+    total = "0";
   } else {
-    total = totalNum.innerText
-    total = total.replace(/,/g, '');
+    total = totalNum.innerText;
+    total = total.replace(/,/g, "");
     total = total.slice(0, -1);
-    if (total === '-') {
-      total = '0';
+    if (total === "-") {
+      total = "0";
       totalNum.innerText = total;
     } else {
       if (!(/[\.]/.test(total))) {
@@ -173,22 +179,22 @@ del.addEventListener("click", (event) => {
       }
       totalNum.innerText = total;
       firstNum = total;
-      secondNum = '';
-      sign = '';
-      signTwo = '';
-      total = '';
+      secondNum = "";
+      sign = "";
+      signTwo = "";
+      total = "";
     }
   }
 })
 
 reset.addEventListener("click", (event) => {
   event.preventDefault;
-  totalNum.innerText = '0'
-  total = '0'
-  firstNum = '';
-  secondNum = '';
-  totalAmount = '';
-  formattedAmount = '';
-  sign = '';
-  signTwo = '';
+  totalNum.innerText = "0";
+  total = "0";
+  firstNum = "";
+  secondNum = "";
+  formattedAmount = "";
+  sign = "";
+  signTwo = "";
+  check = [];
 })
